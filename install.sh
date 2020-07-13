66efd024e14f213eacf4cbf7c10baf176d68de0a
Su Yue <Damenly_Su@gmx.com>
2020-07-13 16:55:06 +0800

    Use `curl https://wtfismyip.com/text` to fetch ip
    Signed-off-by: Su Yue <Damenly_Su@gmx.com>

--- install.sh
+++ install.sh
@@ -467,7 +467,7 @@ domain_check() {
     [[ -z "$domain" ]] && read -rp "请输入你的域名信息(eg:www.damenly.com):" domain
     domain_ip=$(ping "${domain}" -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
     echo -e "${OK} ${GreenBG} 正在获取 公网ip 信息，请耐心等待 ${Font}"
-    local_ip=$(curl -4 ip.sb)
+    local_ip=$(curl https://wtfismyip.com/text)
     echo -e "域名dns解析IP：${domain_ip}"
     echo -e "本机IP: ${local_ip}"
     sleep 2
