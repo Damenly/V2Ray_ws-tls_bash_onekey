217fe70b44894536db9955fcdb6fec5d3f741d27
Su Yue <Damenly_Su@gmx.com>
2020-07-14 12:17:28 +0800

    Stop nginx before acme
    Signed-off-by: Su Yue <Damenly_Su@gmx.com>

--- install.sh
+++ install.sh
@@ -505,6 +505,8 @@ port_exist_check() {
     fi
 }
 acme() {
+    systemctl stop nginx
+    
     if "$HOME"/.acme.sh/acme.sh --issue -d "${domain}" --standalone -k ec-256 --force --test; then
         echo -e "${OK} ${GreenBG} SSL 证书测试签发成功，开始正式签发 ${Font}"
         rm -rf "$HOME/.acme.sh/${domain}_ecc"
